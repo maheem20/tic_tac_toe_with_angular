4c18e3b0dc427dff4262cdee6313a120b28c1d99
maheem_20 <mwistanbuly@gmail.com>
2023-07-18 11:19:05 +0200

    initialize boolean
    to keep track of current player

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoardComponent implements OnInit {
   squares: any[] | undefined;
+  xIsNext: boolean | undefined;
 
   constructor() { }
 
