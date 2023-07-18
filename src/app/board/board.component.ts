bd602f727c07b86445ed08115f449bc842f7bf5d
maheem_20 <mwistanbuly@gmail.com>
2023-07-18 11:20:34 +0200

    initialize string
    to hold name of winner (if there is one)

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class BoardComponent implements OnInit {
   squares: any[] | undefined;
   xIsNext: boolean | undefined;
+  winner: string | undefined;
 
   constructor() { }
 
