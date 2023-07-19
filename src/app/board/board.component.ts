d216d61b81c0c218d0eff853278466bbd3649452
maheem_20 <mwistanbuly@gmail.com>
2023-07-19 23:49:32 +0200

    toggle xIsNext to switch players

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -29,6 +29,7 @@ export class BoardComponent implements OnInit {
   makeMove(idx: number) {
     if (!this.squares[idx]) {
       this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
     }
   }
 
