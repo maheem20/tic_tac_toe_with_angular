cc529d835cd3571d39ba6bc57f336e3e9f71e855
Maheem <88165826+maheem20@users.noreply.github.com>
2023-07-20 21:35:33 +0000

    check combinations
    1. if first square in combination is not null
    
    2. if first and second squares are equal
    
    3. if first and third squares are equal
    
    return symbol of winning player

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -49,8 +49,15 @@ export class BoardComponent implements OnInit {
 
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
-    }
 
+      if (
+        this.squares![a] &&
+        this.squares![a] === this.squares![b] &&
+        this.squares![a] === this.squares![c]
+        ) {
+        return this.squares![a];
+      }
+    }
   }
 
 }
